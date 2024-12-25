56c8beccde2536a1f5e0586b3bccaf9d5c0ed488
Z3R0zz <123938225+Z3R0zz@users.noreply.github.com>
2024-12-25 16:42:45 +0200

    Add loading embed and total member count to info command response

--- src/commands/categories/utils/info.go
+++ src/commands/categories/utils/info.go
@@ -24,6 +24,21 @@ func init() {
 }
 
 func handleInfo(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	loadingEmbed := types.NewEmbed().
+		SetTitle("Loading...").
+		SetColor(0x4e5454).MessageEmbed
+
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Embeds: []*discordgo.MessageEmbed{loadingEmbed},
+		},
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	info, ok := debug.ReadBuildInfo()
 	if !ok {
 		fmt.Println("Failed to retrieve build information")
@@ -36,45 +51,49 @@ func handleInfo(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		return
 	}
 
+	totalMembers, err := utils.GetTotalUserCount(s) // This takes a while. Ideally I would cache this using Redis or something similar.
+	if err != nil {
+		log.Printf("Failed to get guild user count: %v", err)
+		return
+	}
+
 	embed := types.NewEmbed().
 		SetTitle("✨ Made by resynced.design").
 		AddField("⚙️ Go Version", info.GoVersion, true).
 		AddField("⏰ Uptime", config.FormattedUptime(), true).
 		AddField("🏰 Guild Count", fmt.Sprintf("%d", totalGuilds), true).
+		AddField("👥 Member Count", fmt.Sprintf("%d", totalMembers), true).
 		SetImage("https://r2.resynced.design/cdn/01JFT00BNQ2R8K4DSVNZKY0R4H.png").
 		SetColor(0x4e5454).MessageEmbed
 
-	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
-		Data: &discordgo.InteractionResponseData{
-			Embeds: []*discordgo.MessageEmbed{embed},
-			Components: []discordgo.MessageComponent{
-				discordgo.ActionsRow{
-					Components: []discordgo.MessageComponent{
-						discordgo.Button{
-							Emoji: &discordgo.ComponentEmoji{
-								Name: "🌐",
-							},
-							Label: "Website",
-							Style: discordgo.LinkButton,
-							URL:   "https://resynced.design",
+	_, err = s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
+		Embeds: &[]*discordgo.MessageEmbed{embed},
+		Components: &[]discordgo.MessageComponent{
+			discordgo.ActionsRow{
+				Components: []discordgo.MessageComponent{
+					discordgo.Button{
+						Emoji: &discordgo.ComponentEmoji{
+							Name: "🌐",
 						},
-						discordgo.Button{
-							Emoji: &discordgo.ComponentEmoji{
-								Name: "🔗",
-							},
-							Label: "Github",
-							Style: discordgo.LinkButton,
-							URL:   "https://github.com/ResyncedDesign",
+						Label: "Website",
+						Style: discordgo.LinkButton,
+						URL:   "https://resynced.design",
+					},
+					discordgo.Button{
+						Emoji: &discordgo.ComponentEmoji{
+							Name: "🔗",
 						},
-						discordgo.Button{
-							Emoji: &discordgo.ComponentEmoji{
-								Name: "📜",
-							},
-							Label: "Documentation",
-							Style: discordgo.LinkButton,
-							URL:   "https://docs.resynced.design/discord-go-bot/introduction",
+						Label: "Github",
+						Style: discordgo.LinkButton,
+						URL:   "https://github.com/ResyncedDesign",
+					},
+					discordgo.Button{
+						Emoji: &discordgo.ComponentEmoji{
+							Name: "📜",
 						},
+						Label: "Documentation",
+						Style: discordgo.LinkButton,
+						URL:   "https://docs.resynced.design/discord-go-bot/introduction",
 					},
 				},
 			},
--- src/utils/utils.go
+++ src/utils/utils.go
@@ -24,3 +24,31 @@ func GetGuildCount(s *discordgo.Session) (int, error) {
 
 	return len(allGuilds), nil
 }
+
+func GetTotalUserCount(s *discordgo.Session) (int, error) {
+	var totalMembers int
+	var lastID string
+
+	for {
+		guilds, err := s.UserGuilds(200, "", lastID, false)
+		if err != nil {
+			return 0, err
+		}
+
+		for _, guild := range guilds {
+			g, err := s.GuildWithCounts(guild.ID)
+			if err != nil {
+				return 0, err
+			}
+			totalMembers += g.ApproximateMemberCount
+		}
+
+		if len(guilds) < 200 {
+			break
+		}
+
+		lastID = guilds[len(guilds)-1].ID
+	}
+
+	return totalMembers, nil
+}
